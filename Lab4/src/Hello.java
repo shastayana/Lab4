c5e5e533b0654e9779be4595bfcdef064c12d382
Rhode <Rhode@FrootLoops>
2020-04-17 20:45:34 -0700

    David - Added line for my name

--- Lab4/src/Hello.java
+++ Lab4/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("301173938 - This line was added by Yana Hawkins.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301523690 - This line was added by David Fox.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
